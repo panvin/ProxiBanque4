3e50a96208fa513972b5781c54c4fb5c7995b9fb
Brice Tardy <tardy.brice@hotmail.fr>
2016-10-10 16:08:51 +0200

    nouveau passage a MVC

---
+++ ProxiBanqueTestPom/src/main/java/com/sbev/proxibanque/controller/HomePageController.java
@@ -0,0 +1,70 @@
+package com.sbev.proxibanque.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import com.sbev.proxibanque.domaine.Client;
+import com.sbev.proxibanque.service.ClientService;
+import com.sbev.proxibanque.service.CompteService;
+import com.sbev.proxibanque.service.ConseillerService;
+import com.sbev.proxibanque.service.GerantService;
+
+@Controller
+public class HomePageController extends AbstractController {
+	
+	@Autowired
+	private ClientService clientService;
+	@Autowired
+	private ConseillerService conseillerService;
+	@Autowired
+	private GerantService gerantService; 
+	@Autowired
+	private CompteService compteService; 
+	
+	
+
+	public ClientService getClientService() {
+		return clientService;
+	}
+
+	public void setClientService(ClientService clientService) {
+		this.clientService = clientService;
+	}
+
+	public ConseillerService getConseillerService() {
+		return conseillerService;
+	}
+
+	public void setConseillerService(ConseillerService conseillerService) {
+		this.conseillerService = conseillerService;
+	}
+
+	public GerantService getGerantService() {
+		return gerantService;
+	}
+
+	public void setGerantService(GerantService gerantService) {
+		this.gerantService = gerantService;
+	}
+
+	public CompteService getCompteService() {
+		return compteService;
+	}
+
+	public void setCompteService(CompteService compteService) {
+		this.compteService = compteService;
+	}
+	
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+	List<Client> listeClient = clientService.lireToutClient();
+	return new ModelAndView("clients", "clients", listeClient);
+}
+}
