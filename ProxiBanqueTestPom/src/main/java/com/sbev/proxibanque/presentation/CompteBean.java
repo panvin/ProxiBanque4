167ebdbc1a47554d6889b91b6105eb486a7774f7
Elise <elise.patteyn@gmail.com>
2016-10-11 15:51:12 +0200

    Virement.xhtml + compteBean

--- ProxiBanqueTestPom/src/main/java/com/sbev/proxibanque/presentation/CompteBean.java
+++ ProxiBanqueTestPom/src/main/java/com/sbev/proxibanque/presentation/CompteBean.java
@@ -21,21 +21,81 @@ public class CompteBean implements Serializable{
 	
 	@Autowired
 	private CompteService compteService;
+	private int idcomptedeb, idcomptecred;
+	private double montant;
 	
-	
-	private Compte compte;
+	private Compte compteDeb;
+	private Compte compteCred;
 	
 	
 	public CompteBean() {
 		super();
 	}
 	
+	
 //	private double soldeCourant = (double) compteservice.read(iDCourant).getSolde();
 	
-	public Compte getCompte() {
-		return compte;
+	public CompteService getCompteService() {
+		return compteService;
 	}
-	public void setCompte(Compte compte) {
-		this.compte = compte;
+
+
+	public void setCompteService(CompteService compteService) {
+		this.compteService = compteService;
+	}
+
+
+	public int getIdcomptedeb() {
+		return idcomptedeb;
+	}
+
+
+	public void setIdcomptedeb(int idcomptedeb) {
+		this.idcomptedeb = idcomptedeb;
+	}
+
+
+	public int getIdcomptecred() {
+		return idcomptecred;
+	}
+
+
+	public void setIdcomptecred(int idcomptecred) {
+		this.idcomptecred = idcomptecred;
+	}
+
+
+	public double getMontant() {
+		return montant;
+	}
+
+
+	public void setMontant(double montant) {
+		this.montant = montant;
+	}
+
+	public Compte getCompteDeb() {
+		return compteDeb;
+	}
+
+
+	public void setCompteDeb(Compte compteDeb) {
+		this.compteDeb = compteDeb;
+	}
+
+
+	public Compte getCompteCred() {
+		return compteCred;
+	}
+
+
+	public void setCompteCred(Compte compteCred) {
+		this.compteCred = compteCred;
+	}
+
+
+	public String faireVirement() {
+		compteService.virement(idcomptedeb, idcomptecred, montant);
+		return "conseiller/clients";
 	}
 }
